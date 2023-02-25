2204e8e7d101a2eeb0292f1260f1a92b2578c657
mariatozi <121465897+mariatozi@users.noreply.github.com>
2023-02-24 22:40:23 -0300

    Update guess the number.js

--- guess the number.js
+++ guess the number.js
@@ -1,39 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Guess the number</title>
+  </head>
+  <body onload="init()">
+    <main>
+      <div id="welcomeScreen">
+        <h2>Guess the Number <br />1 - 100</h2>
+      </div>
 
-let computerNumber
-let userNumbers = []
-let attempts = 0
-let maxGuesses = 10
-
-function init() {
-   computerNumber = Math.floor(Math.random() * 100 + 1)
-   console.log(computerNumber)
-}
-
-function compareNumbers() {
-    const userNumber = Number(document.getElementById('inputBox').value)
-    userNumbers.push(' ' + userNumber)
-    document.getElementById('guesses').innerHTML = userNumbers
-
-    if (attempts < maxGuesses) {
-        if (userNumber > computerNumber) {
-            document.getElementById('textOutput').innerHTML = 'Your number is too high'
-            document.getElementById('inputBox').value = ''
-            attempts++       
-            document.getElementById('attempts').innerHTML = attempts
-        }
-        else if (userNumber < computerNumber) {
-            document.getElementById('textOutput').innerHTML = 'Your number is too low'
-            document.getElementById('inputBox').value = ''       
-            attempts++
-            document.getElementById('attempts').innerHTML = attempts
-        }
-        else {
-            document.getElementById('textOutput').innerHTML = 'Congratulations !!!'
-            attempts++
-            document.getElementById('attempts').innerHTML = attempts
-        }
-    }
-    else {
-        document.getElementById('textOutput').innerHTML = 'You Lose :(  The computer number was ' + computerNumber
-    }
-}
+      <div id="gameArea">
+        <section>
+          <h3 id="textOutput">Your Guess:</h3>
+          <input type="number" id="inputBox" min="1" max="100" onchange="compareNumbers()"/>
+        </section>
+        <section class="stats">
+          <div class="info">
+            <p>Number of attempts:</p>
+            <span id="attempts"></span>
+          </div>
+          <div class="info">
+            <p>Your guesses:</p>
+            <span id="guesses"></span>
+          </div>
+        </section>
+        <button id="newGameButton" onclick="newGame()">NEW GAME</button>
+      </div>
+    </main>
+    <script src="index.js"></script>
+  </body>
+</html>
