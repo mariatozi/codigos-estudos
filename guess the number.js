e19b368638951f3243ea64a02c11607df9f6f8f2
mariatozi <121465897+mariatozi@users.noreply.github.com>
2023-02-24 22:34:19 -0300

    Update and rename guess the number to guess the number.js

---
+++ guess the number.js
@@ -0,0 +1,39 @@
+
+let computerNumber
+let userNumbers = []
+let attempts = 0
+let maxGuesses = 10
+
+function init() {
+   computerNumber = Math.floor(Math.random() * 100 + 1)
+   console.log(computerNumber)
+}
+
+function compareNumbers() {
+    const userNumber = Number(document.getElementById('inputBox').value)
+    userNumbers.push(' ' + userNumber)
+    document.getElementById('guesses').innerHTML = userNumbers
+
+    if (attempts < maxGuesses) {
+        if (userNumber > computerNumber) {
+            document.getElementById('textOutput').innerHTML = 'Your number is too high'
+            document.getElementById('inputBox').value = ''
+            attempts++       
+            document.getElementById('attempts').innerHTML = attempts
+        }
+        else if (userNumber < computerNumber) {
+            document.getElementById('textOutput').innerHTML = 'Your number is too low'
+            document.getElementById('inputBox').value = ''       
+            attempts++
+            document.getElementById('attempts').innerHTML = attempts
+        }
+        else {
+            document.getElementById('textOutput').innerHTML = 'Congratulations !!!'
+            attempts++
+            document.getElementById('attempts').innerHTML = attempts
+        }
+    }
+    else {
+        document.getElementById('textOutput').innerHTML = 'You Lose :(  The computer number was ' + computerNumber
+    }
+}
